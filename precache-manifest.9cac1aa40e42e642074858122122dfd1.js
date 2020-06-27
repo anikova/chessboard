c2267d846069f7be8267fb8baa2d49330002f9c4
“ani.aleksandraaa@gmail.com” <ani.aleksandraaa@gmail.com>
2020-06-27 16:00:43 +0200

    Updates

---
+++ precache-manifest.9cac1aa40e42e642074858122122dfd1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57e28cce4e04d8f67b86079be908a10b",
+    "url": "/chessboard/index.html"
+  },
+  {
+    "revision": "90419cfea722659a0756",
+    "url": "/chessboard/static/css/main.0a227942.chunk.css"
+  },
+  {
+    "revision": "d1dae190d39cbafe9a0f",
+    "url": "/chessboard/static/js/2.a875ee7b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/chessboard/static/js/2.a875ee7b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "90419cfea722659a0756",
+    "url": "/chessboard/static/js/main.5c2b56e6.chunk.js"
+  },
+  {
+    "revision": "0d3c40f60127b8e0b0eb",
+    "url": "/chessboard/static/js/runtime-main.168dfede.js"
+  }
+]);
